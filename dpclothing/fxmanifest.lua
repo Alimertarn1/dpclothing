cfad432b5c588d06c537288ca8a372e205629070
Alimertarn1 <58050280+Alimertarn1@users.noreply.github.com>
2020-06-15 21:05:04 +0300

    Add files via upload

---
+++ dpclothing/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+game 'gta5'
+
+client_scripts {
+	'client/functions.lua', 		-- Global Functions / Events / Debug and Locale start.
+	'locale/*.lua', 				-- Locales.
+	'client/config.lua',			-- Configuration.
+	'client/variations.lua',		-- Variants, this is where you wanan change stuff around most likely.
+	'client/clothing.lua',
+	'client/GUI.lua',				-- The GUI.
+}
